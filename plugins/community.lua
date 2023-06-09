e1957a4ab4a87612440ad88b3c9163b4d34cf839
Willem Meints <1550763+wmeints@users.noreply.github.com>
2023-06-09 12:02:25 +0200

    Add copilot plugin

---
+++ plugins/community.lua
@@ -0,0 +1,19 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.completion.copilot-lua" },
+  { -- further customize the options set by the community
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  }
+}
