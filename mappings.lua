77183d84918319c0282880f581d36083c463895e
Willem Meints <1550763+wmeints@users.noreply.github.com>
2023-06-10 10:48:44 +0200

    Add Rest client plugin

---
+++ mappings.lua
@@ -0,0 +1,7 @@
+return {
+  n = {
+    ["<leader>rr"] = { "<Plug>RestNvim<CR>", desc = "Run request under the cursor" },
+    ["<leader>rp"] = { "<Plug>RestNvimPreview", desc = "Preview request under the cursor" },
+    ["<leader>rl"] = { "<Plug>RestNvimLast<CR>", desc = "Re-run the last request" }
+  }
+}
