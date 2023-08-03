4b10dc6a8301af2f82d6c9bf5de9815190ad9dcd
Willem Meints <1550763+wmeints@users.noreply.github.com>
2023-08-03 07:42:08 +0200

    Add treesitter configuration

---
+++ plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    -- add more things to the ensure_installed table protecting against community packs modifying it
+    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+      "lua",
+      "c_sharp",
+      "javascript",
+      "typescript",
+      "css",
+      "python",
+      "json"
+    })
+
+    opts.auto_install = true
+  end,
+}
