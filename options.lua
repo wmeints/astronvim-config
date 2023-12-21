ccd507d6f7c73d6c503a2d1fcd4ddbf727d3f0f0
Willem Meints <1550763+wmeints@users.noreply.github.com>
2023-12-21 19:06:57 +0100

    Add tokyonight as the default color scheme

---
+++ options.lua
@@ -0,0 +1,7 @@
+return {
+  g = {
+    loaded_perl_provider = 0,
+    loaded_ruby_provider = 0,
+    colorscheme = "astrodark",
+  },
+}
--- plugins/community.lua
+++ plugins/community.lua
@@ -1,5 +1,6 @@
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.completion.copilot-lua" },
   { -- further customize the options set by the community
      "copilot.lua",
