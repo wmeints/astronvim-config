136358d1646bbf334d1bd637032738c632203524
Willem Meints <1550763+wmeints@users.noreply.github.com>
2024-05-16 07:50:41 +0200

    Initial commit

---
+++ lua/polish.lua
@@ -0,0 +1,7 @@
+-- This will run last in the setup process and is a good place to configure
+-- things like custom filetypes. This just pure lua so anything that doesn't
+-- fit in the normal config locations above can go here
+
+
+-- Set the scrolloff to 5
+vim.opt.scrolloff = 5
