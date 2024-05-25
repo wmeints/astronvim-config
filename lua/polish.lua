59c5124fb30e664aff0590099d86a7951f149f83
Willem Meints <1550763+wmeints@users.noreply.github.com>
2024-05-25 10:23:53 +0200

    Add vim tmux navigator plugin config

--- lua/polish.lua
+++ lua/polish.lua
@@ -2,6 +2,11 @@
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
-
 -- Set the scrolloff to 5
 vim.opt.scrolloff = 5
+
+-- Improve navigation between panes in vim
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
