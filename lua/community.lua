2ff4642b73629443c8c5422781837fdb8db3433e
Willem Meints <1550763+wmeints@users.noreply.github.com>
2024-06-02 07:48:26 +0200

    Add rust language pack

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,3 @@
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -6,6 +5,7 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.markdown"},
-  { import = "astrocommunity.completion.copilot-lua"}
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.completion.copilot-lua" },
 }
