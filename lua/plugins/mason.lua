352317bfb7c9b14d47838c412f055a1b2302934b
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-11-02 10:25:32 +0100

    Update mason configuration

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,27 +1,11 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- Customize Mason
-
 ---@type LazySpec
 return {
-  -- use mason-tool-installer for automatically installing Mason packages
   {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    -- overrides `require("mason-tool-installer").setup(...)`
+    "williamboman/mason.nvim",
     opts = {
-      -- Make sure to use the names found in `:Mason`
-      ensure_installed = {
-        -- install language servers
-        "lua-language-server",
-
-        -- install formatters
-        "stylua",
-
-        -- install debuggers
-        "debugpy",
-
-        -- install any other package
-        "tree-sitter-cli",
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
       },
     },
   },
